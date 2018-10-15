1be5a288b0e42a2bc310288d11a998c5e3427ecf
LilliamHaro <qualia950@gmail.com>
2018-10-14 19:23:37 -0500

    base

---
+++ main.js
@@ -0,0 +1,23 @@
+ var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      // init: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+      }
+    });
